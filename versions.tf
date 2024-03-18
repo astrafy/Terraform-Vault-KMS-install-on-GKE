b0118e2d2f81e978bc3fdff9ab5a2afb9f21beb4
Alejandro De La Cruz López <alejandro@astrafy.io>
2024-03-18 14:47:39 +0100

    upgraded

--- versions.tf
+++ versions.tf
@@ -4,15 +4,15 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">=4.47.0, <5.0.0"
+      version = ">=4.47.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">=2.6.0, <3.0.0"
+      version = ">=2.6.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">=2.14.0, <3.0.0"
+      version = ">=2.14.0"
     }
   }
 
